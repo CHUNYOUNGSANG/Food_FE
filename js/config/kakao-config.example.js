9446213e3e27339bf1cf4c32decbdad55f241ac7
kwonminsung0 <minsung21c@gmail.com>
2026-02-23 23:53:16 +0900

    kakao-config 추가

---
+++ js/config/kakao-config.example.js
@@ -0,0 +1,6 @@
+// 카카오 지도 API 앱 키 설정 예시
+// 이 파일을 복사해서 kakao-config.js 로 이름을 바꾼 뒤,
+// 카카오 개발자센터(https://developers.kakao.com)에서 발급받은 본인의 JavaScript 키를 입력하세요.
+//
+// $ cp js/config/kakao-config.example.js js/config/kakao-config.js
+window.KAKAO_APP_KEY = 'YOUR_KAKAO_JAVASCRIPT_KEY_HERE';
